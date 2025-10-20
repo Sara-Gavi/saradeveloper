0ccf038f336a24a34b48fc1276b84913dfb12157
Sara Gavilán <146012936+Sara-Gavi@users.noreply.github.com>
2025-10-20 17:01:38 +0200

    build: project ready for Vercel deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
-          crossOrigin="true"
+          crossOrigin="anonymous"
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
